58eab0dc3a80c80e2a3aadb21a8cdd575fe5204b
JoschuaGoetz <joschua.goetz@gmail.com>
2020-06-14 17:04:07 +0200

    Add comments for the integration test executable file. Add presentation

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -2,7 +2,11 @@ import 'package:flutter_driver/driver_extension.dart';
 import 'package:sepokedex/main.dart' as app;
 
 void main() {
+  // Enable the flutter driver extension to run and capture tests
   enableFlutterDriverExtension();
 
+  // Configure things here before the app is launched.
+
+  // Execute the main app
   app.main();
 }
